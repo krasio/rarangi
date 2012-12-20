237ecb291a6fcbe9e0875bf72d023a3e16fab3f7
Krasimir Angelov <krasio@codingspree.net>
2012-12-20 14:38:02 +0200

    Bundlerize

--- lib/rarangi.rb
+++ lib/rarangi.rb
@@ -1,6 +1,6 @@
-$: << File.dirname(__FILE__)
-require 'rarangi/list'
-require 'rarangi/entry'
+require "rarangi/version"
+require "rarangi/list"
+require "rarangi/entry"
 
 module Rarangi
 end
---
+++ lib/rarangi/version.rb
@@ -0,0 +1,3 @@
+module Rarangi
+  VERSION = "0.0.1"
+end
