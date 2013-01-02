de6cc0a143fd2ea616216ec4d4cb4e7c2fb67757
Krasimir Angelov <krasio@codingspree.net>
2013-01-02 17:22:54 +0200

    Refactor user steps

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,10 +1,10 @@
 Given /^I'm a new user$/ do
-  @me = create_user
+  @me = create_user('krasio')
   @list = @me.list
 end
 
 Given /^there is user named "(.*?)"$/ do |name|
-  Rarangi::CreateUser.new.exec(name)
+  create_user(name)
 end
 
 Then /^"(.*?)" should exists$/ do |name|
@@ -15,18 +15,6 @@ Then /^"(.*?)" should have a list$/ do |name|
   expect(Repository.users.find_by_name(name).list).to be
 end
 
-def create_user
-  user = Rarangi::User.new
-  Repository.users.create(user)
-  create_list(user)
-
-  user
-end
-
-def create_list(user)
-  list = Rarangi::List.new(owner_id: user.id)
-  Repository.lists.create(list)
-  user.list = list
-
-  list
+def create_user(name)
+  Rarangi::CreateUser.new.exec(name)
 end
